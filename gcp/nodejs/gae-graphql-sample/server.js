d512baa3a62600451bb3e6f665bc8598e0caff83
Takafumi Umemoto <umecch@gmail.com>
2018-06-15 00:30:41 +0900

    [gcp/nodejs/gae-graphql-sample] findTodo/listTodos Queryを実装

--- gcp/nodejs/gae-graphql-sample/server.js
+++ gcp/nodejs/gae-graphql-sample/server.js
@@ -2,19 +2,49 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const graphql = require('graphql');
 
+let database = [
+  {
+    id: 'a',
+    name: 'task1',
+  },
+  {
+    id: 'b',
+    name: 'task2',
+  },
+];
+
+const TodoType = new graphql.GraphQLObjectType({
+  name: 'Todo',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString }
+  }
+})
+
 const queryType = new graphql.GraphQLObjectType({
   name: 'Query',
   fields: {
-    hello: {
-      type: graphql.GraphQLString,
+    findTodo: {
+      type: TodoType,
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: (_, {id}) => {
+        return database.find(data=> data.id === id)
+      }
+    },
+    listTodos: {
+      type: new graphql.GraphQLList(TodoType),
       resolve: () => {
-        return 'Hello world!'
+        return database
       }
     }
   }
 })
 
-const schema = new graphql.GraphQLSchema({query: queryType});
+const schema = new graphql.GraphQLSchema({
+  query: queryType
+});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
