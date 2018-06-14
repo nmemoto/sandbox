011cf709f087d2bbb9205f0cf653d5f2de29f356
Takafumi Umemoto <umecch@gmail.com>
2018-06-14 23:07:21 +0900

    [gcp/nodejs/gae-graphql-sample] graphql.GraphQLSchemaとgraphql.GraphQLObjectTypeを使って修正

--- gcp/nodejs/gae-graphql-sample/server.js
+++ gcp/nodejs/gae-graphql-sample/server.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const graphql = require('graphql');
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    hello: {
+      type: graphql.GraphQLString,
+      resolve: () => {
+        return 'Hello world!'
+      }
+    }
   }
-`);
+})
 
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+const schema = new graphql.GraphQLSchema({query: queryType});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 
